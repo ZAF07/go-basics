8be9ec4364e4e514e6a66295530b4a71152e1098
ZAF07 <zaffere07@gmail.com>
2023-07-30 23:37:19 +0800

    added example of goroutine usage

---
+++ concurrency/concurrent_test.go
@@ -0,0 +1,20 @@
+package concurrency
+
+import "testing"
+
+var result = [][]int{}
+
+func BenchmarkRoutine(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		result = RoutineMethod()
+	}
+}
+
+var nResult = [][]int{}
+
+func BenchmarkNormal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		nResult = NoRoutine()
+	}
+
+}
--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"fmt"
-
-	entity "github.com/ZAF07/go-basics/composition/living-entity"
-	singleton "github.com/ZAF07/go-basics/sync/sync-once"
+	"github.com/ZAF07/go-basics/concurrency"
 )
 
+// entity "github.com/ZAF07/go-basics/composition/living-entity"
+// singleton "github.com/ZAF07/go-basics/sync/sync-once"
+
 func main() {
-	shark := entity.NewBiter("sharkboy", 2)
-	human := entity.NewNonBiter("human guy")
-	fmt.Println(shark)
-	fmt.Println(human)
+	// shark := entity.NewBiter("sharkboy", 2)
+	// human := entity.NewNonBiter("human guy")
+	// fmt.Println(shark)
+	// fmt.Println(human)
 
-	shark.Bite(human)
-	fmt.Println(human)
-	humanName := human.GetName() // If both the embedded struct and the struct that embeds the struct has the same method, Go will usee the struct's method instead
-	fmt.Println(humanName)
+	// shark.Bite(human)
+	// fmt.Println(human)
+	// humanName := human.GetName() // If both the embedded struct and the struct that embeds the struct has the same method, Go will usee the struct's method instead
+	// fmt.Println(humanName)
 
-	// SYNC.ONCE
-	// Call the NewPewrson function. This causes sync.Once to register that the NewPerson has ran ONE TIME. It will NOT run again
-	p1 := singleton.NewPerson()
-	// We can prove that by changing the name value of the person singleton and call NewPerson() again. We will see that we are in fact acting on the same instance that was returned when calling NewPerson() the first time
-	p1.Name = "James"
-	fmt.Println(p1)
+	// // SYNC.ONCE
+	// // Call the NewPewrson function. This causes sync.Once to register that the NewPerson has ran ONE TIME. It will NOT run again
+	// p1 := singleton.NewPerson()
+	// // We can prove that by changing the name value of the person singleton and call NewPerson() again. We will see that we are in fact acting on the same instance that was returned when calling NewPerson() the first time
+	// p1.Name = "James"
+	// fmt.Println(p1)
 
-	// Calling the second time will NOT execute the inner function of the NewPerson(). We can see that the 'NewPerson Ran' was only printed once
-	p2 := singleton.NewPerson()
-	fmt.Println(p2)
+	// // Calling the second time will NOT execute the inner function of the NewPerson(). We can see that the 'NewPerson Ran' was only printed once
+	// p2 := singleton.NewPerson()
+	// fmt.Println(p2)
+
+	concurrency.RoutineMethod()
 }
