0132336a4c260a27e9f6dee0aaf8e6d0adf5e3ab
ZAF07 <zaffere07@gmail.com>
2024-03-26 12:23:44 +0800

    added comments to explain precedence over struct methods in embeddings

--- composition/non-biter/non-biter.go
+++ composition/non-biter/non-biter.go
@@ -8,7 +8,10 @@ type NonBiter struct {
 	entity.ILivingEntity // Embedded anonymous field containing the entity struct. This makes the NonBiter struct looks like it inherits from the entity stuct. But it does not! It only allows us to call properties and methods of the embedded struct like it belongs to the child struct
 }
 
-// Notice that the entity struct also implements the GetName() method. If both embedded and child struct implements the same methods, then the child struct's method will take precedence over the embedded struct's method
+/*
+	Notice that the NonBiter struct also implements the GetName() method.
+	If both embedded and embedding struct implements the same methods, then the embedding struct's method will take precedence over the embedded struct's method
+*/
 // func (n *NonBiter) GetName() string {
 // 	return "Haha"
 // }
