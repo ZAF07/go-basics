4585701e851350953520514bbbe1d208310debfd
ZAF07 <zaffere07@gmail.com>
2024-03-27 01:48:03 +0800

    edited main.go to be playground for golang

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-import (
-	"container/heap"
-	"fmt"
-
-	q "github.com/ZAF07/go-basics/priority-queue"
-)
-
 // entity "github.com/ZAF07/go-basics/composition/living-entity"
 // singleton "github.com/ZAF07/go-basics/sync/sync-once"
 
 func main() {
+	/* THIS IS A PLAYGROUND. PERFORM ANYTING GO RELATED */
 
+	// TODO: Add below to its own main package
 	// // SYNC.ONCE
 	// // Call the NewPewrson function. This causes sync.Once to register that the NewPerson has ran ONE TIME. It will NOT run again
 	// p1 := singleton.NewPerson()
@@ -23,43 +18,4 @@ func main() {
 	// p2 := singleton.NewPerson()
 	// fmt.Println(p2)
 
-	// PQ (unrelated to concurrency)
-	w := q.NewPQ()
-	fmt.Println("Before push: ", w)
-	a := q.PriorityItem{
-		Value:   3,
-		StringV: "3",
-	}
-
-	b := q.PriorityItem{
-		Value:   1,
-		StringV: "11",
-	}
-
-	c := q.PriorityItem{
-		Value:   4,
-		StringV: "4",
-	}
-
-	d := q.PriorityItem{
-		Value:   2,
-		StringV: "66",
-	}
-
-	heap.Push(w, &a)
-	heap.Push(w, &b)
-	heap.Push(w, &c)
-	heap.Push(w, &d)
-
-	fmt.Printf("after push: %+v\n", w)
-	fmt.Println("peek --> ", w.Peek())
-	ap := w.Pop()
-	bp := w.Pop()
-	cp := w.Pop()
-	dp := w.Pop()
-
-	fmt.Println("❌", ap)
-	fmt.Println("❌", bp)
-	fmt.Println("❌", cp)
-	fmt.Println("❌", dp)
 }
