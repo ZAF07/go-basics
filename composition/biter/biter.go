d62d59fd67268243f34fa00de42a7d4c2f51f923
ZAF07 <zaffere07@gmail.com>
2024-03-30 01:07:25 +0800

    added comments to make explanation clear

--- composition/biter/biter.go
+++ composition/biter/biter.go
@@ -9,7 +9,7 @@ import (
 // Fields are not exported because we want this properties to be private (so nobody from the outside can call them directly, hence reducing vulnerabilities and runtime bugs)
 type biter struct { // Struct is also private, so it cant be used standalone in any case which may introduce bugs
 	biteForce            int
-	entity.ILivingEntity // 💡 Embedded anonymous field containing the entity struct. This makes the Nonbiter struct looks like it inherits from the entity stuct. But it does not! It only allows us to call properties and methods of the embedded struct like it belongs to the child struct
+	entity.ILivingEntity // 💡 Embedded anonymous field containing the entity struct. This makes the Nonbiter struct looks like it inherits from the entity stuct. But it does not! It only allows us to call properties and methods of the embedded struct from the embedding struct as if it belongs to the embedding (outer) struct
 
 	// entity.Entity // 💡 Use this method if you want to explicitly use the entity.Entity struct. The above (👆) field, uses an interface instead. So that makes it more extensible as ANY struct that implements the ILivingEntity interface is allowed to be used
 
